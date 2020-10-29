9299e1844edc0a0d41f3608d34960694c7f4ff04
zenquan <1847867150@qq.com>
2020-10-29 23:08:03 +0800

    fix:增加vue项目测试

---
+++ test/vue-test-demo/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <Button>点击</Button>
+  </div>
+</template>
+
+<script>
+import { Button } from 'element-ui'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: {
+    Button
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
